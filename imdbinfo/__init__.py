da5fa7c5eeda00dc1c93984ccea5d87fffdb22e3
Tiago Veronesi <tveronesi@gmail.com>
2025-07-11 09:30:17 +0200

    Add logging setup and update package version to 0.2.7; change license to GPL

--- imdbinfo/__init__.py
+++ imdbinfo/__init__.py
@@ -1,1 +1,12 @@
 
+import logging
+
+from .services import get_movie, search_title
+
+__all__ = [
+    "get_movie",
+    "search_title",
+]
+
+# setup library logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
