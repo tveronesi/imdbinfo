37250810c3fea4a1a8d38134c9711201d04e9fe9
tiago <tveronesi@gmail.com>
2025-08-07 23:27:50 +0200

    Update usage examples to fetch episodes for multiple series

--- examples/usage_example_episodes.py
+++ examples/usage_example_episodes.py
@@ -1,10 +1,30 @@
 from imdbinfo import get_movie
 from imdbinfo.services import get_episodes
 
-series_main_tile =  get_movie('tt1520211')
-print(f"Movie Title: {series_main_tile.title} ({series_main_tile.year}) - {series_main_tile.imdbId}")
-print(f"Kind: {series_main_tile.kind}")
-print(f"Serires Info: {series_main_tile.info_series or 'N/A'}")
+series_ids = [
+     'tt1520211',  # The Walking Dead
+     'tt0944947',  # Game of Thrones
+     'tt0903747',  # Breaking Bad
+    'tt1838556',  # a movie ... skip it
+     'tt4574334',  # The Mandalorian
+     'tt0071007'   # little house on the prairie
+]
 
-episodes = get_episodes(series_main_tile.imdb_id, 1)  # Fetch episodes for season 1
-print(f"Total Episodes in Season 1: {len(episodes)}")
+
+for series_id in series_ids:
+    series = get_movie(series_id)
+    if not series.is_series():
+        print(f"Series with ID {series_id} not found.")
+        continue
+    print(f"Movie Title: {series.title} ({series.year}) - {series.imdbId}")
+    print(f"Kind: {series.kind}")
+    print(f"Series Info: {series.info_series or 'N/A'}")
+    print("----------------------------------------------")
+    seasons = series.info_series.display_seasons
+    seasons.reverse()
+    for season_number in seasons:
+        season_list = get_episodes(series_id, season_number)
+        print(f"Total Episodes in Season {season_number}: {len(season_list)}")
+        for episode in season_list.episodes:
+            print(f"{episode}")
+    print("\n" + "="*50 + "\n")
