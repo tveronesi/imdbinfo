6f4b9ca6718e94b2a745c5ccc636aa3b1c9335af
Tiago <tveronesi@gmail.com>
2025-08-07 12:39:04 +0200

    Add docstrings to SeriesMixin methods for clarity on series and episode identification

--- imdbinfo/models.py
+++ imdbinfo/models.py
@@ -6,9 +6,17 @@ logger = logging.getLogger(__name__)
 
 class SeriesMixin:
     def is_series(self) -> bool:
+        """
+        Check if this movie title is a series, the main title of a series.
+        If True, it means that this is a series, not a movie, not an episode, but the main reference for the series itself, and series details can be found in the self.info_series property.
+        """
         return getattr(self, "kind", None) in ( "tvSeries","tvMiniSeries")
 
     def is_episode(self) -> bool:
+        """
+        Check if this movie title is an episode of a series.
+        If True, means that this is the episode of a series and episode details can be found in the self.info_episode property
+        """
         return getattr(self, "kind", None) == "tvEpisode"
 
 
