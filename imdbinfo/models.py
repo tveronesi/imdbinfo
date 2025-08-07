c5bd97c9f639b6186d60a62a5192ac999453d68e
tiago <tveronesi@gmail.com>
2025-08-07 23:32:17 +0200

    Add SeriesInfo and EpisodeInfo models with validation and string representation

--- imdbinfo/models.py
+++ imdbinfo/models.py
@@ -19,6 +19,32 @@ class SeriesMixin:
         """
         return getattr(self, "kind", None) == "tvEpisode"
 
+class SeriesInfo(BaseModel):
+    display_years : List[str] = []  # e.g. ['2013', '2014', '2015']
+    display_seasons : List[str] = []  # e.g. ['1', '2', '3']
+
+    @field_validator('display_years', mode='before')
+    def filter_years(cls, value):
+        if value is None:
+            return []
+        return [str(y) for y in value if isinstance(y, str) and len(y) == 4 and y.isdigit()]
+
+    def __str__(self):
+        return f"Years: {self.display_years[-1] if self.display_years else ''}-{self.display_years[0] if self.display_years else ''}, Seasons: {len(self.display_seasons)}"
+
+
+class EpisodeInfo(BaseModel):
+    season_n: Optional[int] = None
+    episode_n: Optional[int] = None
+    series_imdbId: Optional[str] = None
+    series_title: Optional[str] = None
+    series_title_localized: Optional[str] = None
+
+    def __str__(self):
+        # print in S01E01 format
+        season_str = f"S{self.season_n:02d}" if self.season_n is not None else "S??"
+        episode_str = f"E{self.episode_n:02d}" if self.episode_n is not None else "E??"
+        return f"{self.series_title} - {season_str}{episode_str} ({self.series_imdbId})"
 
 class Person(BaseModel):
     """ person model for directors, cast and search results.
@@ -156,14 +182,17 @@ class MovieDetail(SeriesMixin, BaseModel):
     synopses: List[str] = []
     production: List[str] = []
     categories: Dict[str, List[Union[Person, CastMember]]] = {}
-    info_series : Optional['SeriesInfo'] = None # e.g. SeriesInfo(display_years=['2013', '2014', '2015'], display_seasons=['1', '2', '3'])
-    info_episode : Optional['EpisodeInfo'] = None # e.g. SeriesInfo(display_year
+    info_series : Optional[SeriesInfo] = None # e.g. SeriesInfo(display_years=['2013', '2014', '2015'], display_seasons=['1', '2', '3'])
+    info_episode : Optional[EpisodeInfo] = None # e.g. SeriesInfo(display_year
 
     @field_validator('languages', 'country_codes','genres', mode='before')
     def none_is_list(cls, value):
         if value is None:
             return []
         return value
+
+    def __str__(self):
+        return f"{self.title} ({self.year}) - {self.imdbId} ({self.kind})"
 
 
 class MovieInfo(SeriesMixin, BaseModel):
@@ -255,36 +284,52 @@ class PersonDetail(BaseModel):
         return f"{self.name} ({', '.join(self.knownfor)})"
 
 
-class SeriesInfo(BaseModel):
-    display_years : List[str] = []  # e.g. ['2013', '2014', '2015']
-    display_seasons : List[str] = []  # e.g. ['1', '2', '3']
 
-    @field_validator('display_years', mode='before')
-    def filter_years(cls, value):
-        if value is None:
-            return []
-        return [str(y) for y in value if isinstance(y, str) and len(y) == 4 and y.isdigit()]
+
+
+class EpisodeData(BaseModel):
+    id: str  # id without 'tt' prefix, e.g. '1234567'
+    imdbId: str
+    imdb_id: str
+    title: str
+    season: int
+    episode: int
+    plot: str
+    cover_url: Optional[str] = None
+    rating: Optional[float] = None
+    votes: Optional[int] = None
+    year: Optional[int] = None
+    release_date: Optional[str] = None
+    kind: Optional[str] = None
+
+    @classmethod
+    def from_episode_data(cls, data: dict) -> 'EpisodeData':
+        """
+        Create an EpisodeData instance from episode data dictionary.
+        """
+        return cls(
+            id=data['id'].replace('tt', ''),
+            imdbId=data['id'],
+            imdb_id=data['id'].replace('tt', ''),
+            title=data['titleText'],
+            season=data['season'],
+            episode=data['episode'],
+            plot=data.get('plot',''),
+            cover_url=data.get('image', {}).get('url', None),
+            rating=data.get('aggregateRating', None),
+            votes=data.get('voteCount', None),
+            year=data.get('releaseYear', None),
+            release_date="-".join(
+                str(data['releaseDate'].get(k, '')).zfill(2) for k in ['year', 'month', 'day']) if data.get(
+                'releaseDate') else None,
+            kind=data.get('type'),
+
+        )
 
     def __str__(self):
-        return f"Years: {self.display_years[-1]}-{self.display_years[0] or 'still' }, Seasons: {len(self.display_seasons)}"
+        return f"{self.title} (S{self.season:02d}E{self.episode:02d}) - {self.imdbId} ({self.year or 'N/A'}) - {self.kind or 'N/A'}"
 
 
-class EpisodeInfo(BaseModel):
-    season_n: Optional[int] = None
-    episode_n: Optional[int] = None
-    series_imdbId: Optional[str] = None
-    series_title: Optional[str] = None
-    series_title_localized: Optional[str] = None
-
-    def __str__(self):
-        # print in S01E01 format
-        season_str = f"S{self.season_n:02d}" if self.season_n is not None else "S??"
-        episode_str = f"E{self.episode_n:02d}" if self.episode_n is not None else "E??"
-        return f"{self.series_title} - {season_str}{episode_str} ({self.series_imdbId})"
-
-
-class Episode(BaseModel):
-    pass
 
 
 class EpisodesList(BaseModel):
@@ -293,8 +338,26 @@ class EpisodesList(BaseModel):
     This model contains a list of EpisodeInfo objects representing the episodes of a series.
     It can be used to represent the episodes of a series in a specific season.
     """
-    episodes: List[Episode] = []
+    top_rating_episode :  Optional[float] = None
+    total_series_episodes  : Optional[int] = None  # Total number of episodes in the series
+    total_series_seasons : Optional[int] = None  # Total number of seasons in the series
+    top_ten_episodes : Optional[List[dict]]  = None # List of top ten episodes based on rating
+    episodes: List[EpisodeData] = []
 
+    @property
+    def count(self)-> int:
+        """
+        Count the number of episodes in the list.
+        Returns:
+            int: The number of episodes in the list.
+        """
+        return len(self.episodes)
+
+    def __len__(self):
+        return len(self.episodes)
+
+    def __getitem__(self, idx):
+        return self.episodes[idx]
 
     def __str__(self):
         return f"Total Episodes: {len(self.episodes)}"
