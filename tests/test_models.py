b8e1fc8b8d468b8df4d72a54d3af0c8edcf90833
tveronesi+imdbinfo@rog <tveronesi@gmail.com>
2025-09-29 22:28:06 +0200

    refactor: rename from_directors method to from_directors_creators for clarity

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,7 @@ from imdbinfo.models import InfoSeries, InfoEpisode
 
 def test_person_from_directors():
     data = {"name": {"nameText": {"text": "Lana Wachowski"}, "id": "nm0905154"}}
-    person = Person.from_directors(data)
+    person = Person.from_directors_creators(data)
     assert person.name == "Lana Wachowski"
     assert person.imdb_id == "0905154"
     assert person.job == "Director"
